4974543bfeaf6400c2831c57eb827276ee75d601
AnaCroitor <ana.croitor1991@gmail.com>
2025-06-30 21:17:53 +0100

    Updating_Recipe.kt

--- app/src/main/java/co/uk/bbk/anacroitor/Recipe.kt
+++ app/src/main/java/co/uk/bbk/anacroitor/Recipe.kt
@@ -3,11 +3,12 @@ package co.uk.bbk.anacroitor
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+// Room entity which represents a recipe entry
 @Entity(tableName = "recipes")
 data class Recipe(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0, // Auto-incremented primary key
     val title: String,
     val ingredients: String,
     val instructions: String,
-    val category: String
+    val category: String // Category: Breakfast, Brunch, Lunch etc.
 )
