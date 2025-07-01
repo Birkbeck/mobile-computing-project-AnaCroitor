25269669598381b6d9126a016e45f9f1b7ec9451
AnaCroitor <ana.croitor1991@gmail.com>
2025-07-01 19:55:44 +0100

    Updating_AppDatabase.kt

--- app/src/main/java/co/uk/bbk/anacroitor/AppDatabase.kt
+++ app/src/main/java/co/uk/bbk/anacroitor/AppDatabase.kt
@@ -5,14 +5,17 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+// Defines the Room database with the Recipe entity and DAO
 @Database(entities = [Recipe::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
+    // Provides access to DAO methods
     abstract fun recipeDao(): RecipeDao
 
     companion object {
         @Volatile private var INSTANCE: AppDatabase? = null
 
+        // Returns a singleton instance of the database
         fun getInstance(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
                 INSTANCE ?: Room.databaseBuilder(
