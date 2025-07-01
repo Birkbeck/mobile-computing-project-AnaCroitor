b29eec5868398ff87762fc83a44b6e3df3357873
AnaCroitor <ana.croitor1991@gmail.com>
2025-07-01 19:58:31 +0100

    Updating_RecipeRepository.kt

--- app/src/main/java/co/uk/bbk/anacroitor/RecipeRepository.kt
+++ app/src/main/java/co/uk/bbk/anacroitor/RecipeRepository.kt
@@ -1,23 +1,29 @@
 package co.uk.bbk.anacroitor
 
+// Abstracts data access
 class RecipeRepository(private val dao: RecipeDao) {
 
+    // Inserts a new recipe into the database
     suspend fun insert(recipe: Recipe) {
         dao.insert(recipe)
     }
 
+    // Updates an existing recipe
     suspend fun update(recipe: Recipe) {
         dao.update(recipe)
     }
 
+    // Deletes a recipe from the database
     suspend fun delete(recipe: Recipe) {
         dao.delete(recipe)
     }
 
+    // Returns all stored recipes
     suspend fun getAllRecipes(): List<Recipe> {
         return dao.getAllRecipes()
     }
 
+    // Retrieves a recipe by its ID
     suspend fun getRecipeById(id: Int): Recipe? {
         return dao.getRecipeById(id)
     }
